14d1dea17c2917a5ad2c2e32d2015db5945f86ba
Viacheslav Butrim <viacheslavbutrim@gmail.com>
2026-02-16 11:17:11 +0200

    client-fix

---
+++ app/@modal/(.)notes/[id]/NotePreview.client.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import { useParams, useRouter } from "next/navigation";
+import { useQuery } from "@tanstack/react-query";
+import { fetchNoteById } from "@/lib/api";
+import Modal from "@/components/Modal/Modal";
+import css from "./NotePreview.module.css"; // не забути файл сіі для модалки
+import type { Note } from "@/types/note";
+
+export default function NotePreview() {
+  const params = useParams();
+  const router = useRouter();
+  const id = params.id as string;
+
+  const { data: note, isLoading, error } = useQuery<Note>({
+    queryKey: ["note", id],
+    queryFn: () => fetchNoteById(id),
+    refetchOnMount: false,
+  });
+
+  if (isLoading) return <p>Loading...</p>;
+  if (error || !note) return <p>Something went wrong.</p>;
+
+  const handleClose = () => router.back();
+
+  return (
+    <Modal onClose={handleClose}>
+      <div className={css.container}>
+        <h2 className={css.title}>{note.title}</h2>
+        <p className={css.tag}>{note.tag}</p>
+        <p className={css.content}>{note.content}</p>
+        <p className={css.date}>{note.createdAt}</p>
+      </div>
+    </Modal>
+  );
+}
