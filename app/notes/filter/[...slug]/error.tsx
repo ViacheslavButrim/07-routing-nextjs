6fa3d6c63fb3539d944c8a9a43513d009514eabb
Viacheslav Butrim <viacheslavbutrim@gmail.com>
2026-02-16 12:56:03 +0200

    error-fix

---
+++ app/notes/filter/[...slug]/error.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+export default function FilterError() {
+  return (
+    <div style={{ padding: "16px" }}>
+      <h1>Something went wrong</h1>
+      <p>Failed to load filtered notes. Please try again later.</p>
+    </div>
+  );
+}
