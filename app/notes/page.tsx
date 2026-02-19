ebdeaa23c4b30738cb6223f730f33d50094abc8a
Vik <viacheslavbutrim@gmail.com>
2026-02-19 13:13:30 +0200

    Delete app/notes/page.tsx

--- app/notes/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function NotesPage() {
-  return (
-    <div style={{ padding: "40px", textAlign: "center" }}>
-      <h1>Notes Page</h1>
-      <p>This page is required for build.</p>
-    </div>
-  );
-}
